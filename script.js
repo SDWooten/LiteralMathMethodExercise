9deb8bf1e8af76db59e41eadf06108e8da6b8a9d
Shannon Wooten <swooten7448@gmail.com>
2022-09-29 10:40:44 -0500

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,50 @@
+// 1a
+let warmHugs = "Hi \, I\'m Olaf and I like warm hugs.";
+// 1b
+console.log(warmHugs.toUpperCase());
+// 1c
+console.log(warmHugs.replace("like", "LOVE"));
+
+// 2a
+var beenImpaled = "Oh, look at that. I\'ve been impaled.";
+console.log(beenImpaled);
+
+// 2b
+console.log(beenImpaled.slice(18, 36));
+
+// 3a
+let dotDotDot = "...";
+console.log(dotDotDot);
+
+// 3b
+let skullBones = `I don't have a skull${dotDotDot}or bones`;
+console.log(skullBones);
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.floor(Math.random()*3 + 1);
+console.log((randomNumber));
+// FLOOR is best practice!!!
+
+//BONUS
+// 6
+console.log(" Let It Go!".repeat(2).toUpperCase());
+
+// 7a
+let reindeers = "Reindeers are better than people.";
+console.log(reindeers);
+
+// 7b
+console.log(reindeers.replaceAll(" ", "_"));
+console.log(reindeers);
+
+// 8
+console.log(Math.sqrt(2));
+
+// 9
+let newRandomNumber = Math.floor((Math.random()*(17)+7));
+console.log(newRandomNumber);
+
+
